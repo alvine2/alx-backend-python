0d6f807cd0c73411dc50bceaf87d1ae34a3bbc1c
alvine2 <alvinekinyera@gmail.com>
2024-10-10 10:53:11 +0300

    python backend

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that contains  single function that add two integers."""
+
+
+def add(a: float, b: float) -> float:
+    """ Add two integers together."""
+    return a + b
